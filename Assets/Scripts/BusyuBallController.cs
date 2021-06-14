ecde7c36d32dd061722424cc31fcb30f30eb8069
nkc20206055 <ym2022@mailg.denpa.ac.jp>
2021-06-14 09:39:05 +0900

    12日、13日文を追加

---
+++ Assets/Scripts/BusyuBallController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusyuBallController : MonoBehaviour
+{
+    public Sprite[] busyuS;//部首の画像保存用
+    SpriteRenderer SR;//SpriteRenderer保存用変数
+    private int RandomBusyu;//
+    private float x, y;
+    private Vector2 startPos;
+    void Mouse()//スワイプ操作
+    {
+        //スワイプの長さを求める
+        if (Input.GetMouseButtonDown(0))
+        {
+            //マウスをクリックした座標
+            startPos = Input.mousePosition;
+        }else if (Input.GetMouseButtonUp(0))
+        {
+            //マウスを離した座標
+            Vector2 endPos = Input.mousePosition;
+            float Xbusyu = endPos.x - startPos.x;
+            float Ybusyu = endPos.y - startPos.y;
+
+            //スワイプの長さを初速度に変換する
+            x = Xbusyu / 500.0f;
+            y = Ybusyu / 500.0f;
+        }
+        transform.Translate(x, y, 0);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SR = gameObject.GetComponent<SpriteRenderer>();//自分のゲームオブジェクトからSpriteRendererを取得
+        RandomBusyu = Random.Range(0, 5);//どの部首をだすかの数値をランダムにだす
+        SR.sprite = busyuS[RandomBusyu];//RandomBusyuと同じ数値のbusyuSに保存したsprite画像をSRのspriteに保存する
+        Debug.Log(RandomBusyu);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Mouse();
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("当たった");
+        var If = collision.gameObject.GetComponent<Interface>();
+        if (If != null)//当たったオブジェクトにInterfaceがあるか
+        {
+            If.Dameg(RandomBusyu);
+        }
+    }
+}
---
+++ Assets/Scripts/KangeBallController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KangeBallController : MonoBehaviour,Interface
+{
+    public Sprite[] kangis;
+    SpriteRenderer sr;
+    private int kangeRandom;
+    void Interface.Dameg(int i)
+    {
+        //Debug.Log(i);
+        switch (i) 
+        {
+            case 0:
+                switch (kangeRandom)
+                {
+                    case 0:
+                        Debug.Log("当たった 0");
+                        break;
+                }
+                break;
+            case 1:
+                switch (kangeRandom)
+                {
+                    case 1:
+                        Debug.Log("当たった 1");
+                        break;
+                }
+                break;
+            case 2:
+                switch (kangeRandom)
+                {
+                    case 2:
+                        Debug.Log("当たった 2");
+                        break;
+                }
+                break;
+            case 3:
+                switch (kangeRandom)
+                {
+                    case 3:
+                        Debug.Log("当たった 3");
+                        break;
+                }
+                break;
+            case 4:
+                switch (kangeRandom)
+                {
+                    case 4:
+                        Debug.Log("当たった 4");
+                        break;
+                }
+                break;
+        }
+            
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        kangeRandom = Random.Range(0, 5);
+        sr.sprite = kangis[kangeRandom];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
